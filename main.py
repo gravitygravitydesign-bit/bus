f8b3ee13361f0cc2e2e7e59386762b55651578ad
gravitygravitydesign-bit <gravity.gravitydesign@gmail.com>
2026-05-24 14:49:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import flet as ft
 import random
 import string
 import firebase_admin
-from firebase_admin import db, credentials
+from firebase_admin import credentials, firestore
 import time
+import os
 
 # --- إعدادات Firebase ---
-try:
-    if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://gravitybusgame-default-rtdb.europe-west1.firebasedatabase.app'
-        })
-except Exception as e:
-    print(f"Firebase Error: {e}")
+# التعديل الذكي لقراءة الملف داخل الأندرويد بأمان:
+current_dir = os.path.dirname(__file__)
+cred_path = os.path.join(current_dir, "serviceAccountKey.json")
+
+# تأكد أن الاستدعاء يتم بالشكل ده:
+if not firebase_admin._apps:
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 def main(page: ft.Page):
     #page.title = "اتوبيس كومبليت - Gravity Projects"
